3a7f9cde271121269fa1788ba37f10a28fe45dcf
Kelsey <kelseylin2000@gmail.com>
2025-12-02 17:10:13 +0800

    [feat] redirect root to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-
-  // 默認進入根路徑就導向登錄頁 /login
-  redirect("/login");
-
+  // 根路徑直接導向 dashboard
+  redirect("/dashboard");
 }
